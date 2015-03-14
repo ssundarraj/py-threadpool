4a93d17a8959177d69b9477c0a95fadf5be3bd42
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-14 17:50:30 +0530

    Added start function.

--- py_threadpool/utils.py
+++ py_threadpool/utils.py
@@ -5,7 +5,17 @@ class ThreadPool:
 	nhreads = 0
 	q_size = 0
 	job_q = Queue.Queue()
-
+	threads = []
+	
 	def __init__(self, nthreads=10, q_size=0):
 		self.nthreads = nthreads
 		self.q_size = q_size
+
+	def start():
+		for i in range(nthreads):
+			t = threading.Thread(target=worker)
+			threads.append(t)
+			t.start()
+
+	def worker():
+		pass
