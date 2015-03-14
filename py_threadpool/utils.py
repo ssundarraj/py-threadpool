55f499633687a2e6547d18cc179e8336d4ddf0c4
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-14 18:29:47 +0530

    PEP8 fixes.

--- py_threadpool/utils.py
+++ py_threadpool/utils.py
@@ -1,30 +1,31 @@
 import Queue
 import threading
 
+
 class ThreadPool:
-	nhreads = 0
-	q_size = 0
-	job_q = Queue.Queue()
-	threads = []
-	
-	def __init__(self, nthreads=10, q_size=0):
-		self.nthreads = nthreads
-		self.q_size = q_size
+    nhreads = 0
+    q_size = 0
+    job_q = Queue.Queue()
+    threads = []
 
-	def start():
-		for i in range(nthreads):
-			t = threading.Thread(target=worker)
-			threads.append(t)
-			t.start()
-			return True
+    def __init__(self, nthreads=10, q_size=0):
+        self.nthreads = nthreads
+        self.q_size = q_size
 
-	def worker():
-		while job_q.unfinished_jobs:
-			exec_function, args = job_q.get()
-			exec_function(args)
-			job_q.task_done()
-		return True
+    def start():
+        for i in range(nthreads):
+            t = threading.Thread(target=worker)
+            threads.append(t)
+            t.start()
+            return True
 
-	def add_job(exec_function, args):
-		job_q.put((exec_function, args))
-		return True
+    def worker():
+        while job_q.unfinished_jobs:
+            exec_function, args = job_q.get()
+            exec_function(args)
+            job_q.task_done()
+        return True
+
+    def add_job(exec_function, args):
+        job_q.put((exec_function, args))
+        return True
