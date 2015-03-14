0c3d40bd68ef4873b42fe92660218e9c43192553
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-14 18:33:53 +0530

    Added finish, unfinished_tasks, count of jobs.

--- py_threadpool/utils.py
+++ py_threadpool/utils.py
@@ -7,7 +7,7 @@ class ThreadPool:
     q_size = 0
     job_q = Queue.Queue()
     threads = []
-
+    total_jobs = 0
     def __init__(self, nthreads=10, q_size=0):
         self.nthreads = nthreads
         self.q_size = q_size
@@ -28,4 +28,14 @@ class ThreadPool:
 
     def add_job(exec_function, args):
         job_q.put((exec_function, args))
+        total_jobs += 1
         return True
+
+    def finish():
+        job_q.join()
+        return True
+
+    def unfinished_tasks():
+        return job_q.unfinished_tasks
+
+    
