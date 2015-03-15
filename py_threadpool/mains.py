10f398e2cbd506512421c3d14585ad139a566beb
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-15 11:54:47 +0530

    Added demo. Not working for very fast functions.

--- py_threadpool/mains.py
+++ py_threadpool/mains.py
@@ -1,10 +1,13 @@
 from utils import *
+import requests
 
 t = ThreadPool()
 def printer():
-    print "hello\n"
+    requests.get('http://google.com')
+    print "Done"
 
 for i in range(10):
     t.add_job(printer, None)
 
 t.start()
+t.finish()
