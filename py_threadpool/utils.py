496f9ad646c498b30fdc8eb9fc7451e80868b134
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-15 10:42:20 +0530

    Fixed self argument and bugs.

--- py_threadpool/utils.py
+++ py_threadpool/utils.py
@@ -8,34 +8,39 @@ class ThreadPool:
     job_q = Queue.Queue()
     threads = []
     total_jobs = 0
+
     def __init__(self, nthreads=10, q_size=0):
         self.nthreads = nthreads
         self.q_size = q_size
 
-    def start():
-        for i in range(nthreads):
-            t = threading.Thread(target=worker)
-            threads.append(t)
+    def start(self):
+        for i in range(self.nthreads):
+            t = threading.Thread(target=self.worker)
+            self.threads.append(t)
             t.start()
         return True
 
-    def worker():
-        while job_q.unfinished_jobs:
-            exec_function, args = job_q.get()
-            exec_function(args)
-            job_q.task_done()
+    def worker(self):
+        while self.job_q.qsize():
+            exec_function, args = self.job_q.get()
+            if args:
+                exec_function(args)
+            else:
+                exec_function()
+            self.job_q.task_done()
         return True
 
-    def add_job(exec_function, args):
-        job_q.put((exec_function, args))
-        total_jobs += 1
+    def add_job(self, exec_function, args=None):
+        self.job_q.put((exec_function, args))
+        self.total_jobs += 1
         return True
 
-    def finish():
-        job_q.join()
+    def finish(self):
+        self.job_q.join()
         return True
 
-    def unfinished_tasks():
-        return job_q.unfinished_tasks
+    def unfinished_tasks(self):
+        return self.job_q.qsize()
 
-    
+    def finished_tasks(self):
+        return self.total_jobs - self.job_q.qsize()
