49923128ea683ffe992bb245f50653be664bdbb5
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-03-27 22:01:15 +0530

    Better test.

--- pyThreadpool/tester.py
+++ pyThreadpool/tester.py
@@ -2,14 +2,13 @@ from utils import *
 import requests
 
 
-def printer(x, test=None):
-    print x
-    print test
+def printer(x, y, testx=None, testy=None):
+    print x,y,testx,testy
     print "Done"
 
 t = threadpool()
 for i in range(100):
-    j = thread_job(printer, ('formal'), {'test':'keyword'})
+    j = thread_job(printer, ('formalx', 'formaly'), {'testx':'keywordx', 'testy':'keywordy'})
     t.add_job(j)
 t.start()
 t.finish()
