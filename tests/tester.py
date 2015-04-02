43bb14ec5707579507cfbe57023731137e1a1c44
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-04-03 00:54:49 +0530

    Fixed issue with incorrectly named import.

--- tests/tester.py
+++ tests/tester.py
@@ -1,4 +1,4 @@
-import threadpool
+import pyThreadpool
 
 
 def printer(x, y, testx=None, testy=None):
@@ -8,11 +8,11 @@ def printer(x, y, testx=None, testy=None):
     # print a['b']
     return True
 
-t = threadpool.threadpool()
+t = pyThreadpool.threadpool()
 for i in range(100):
     args = ('formalx', 'formaly')
     kwargs = {'testx': 'keywordx', 'testy': 'keywordy'}
-    j = threadpool.thread_job(printer, args, kwargs)
+    j = pyThreadpool.thread_job(printer, args, kwargs)
     t.add_job(j)
 t.start()
 print t.is_active
