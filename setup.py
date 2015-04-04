c7b485d3dab4552ff5cfebe22b57fc925d8338b2
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-04-04 12:56:40 +0530

    Added classification data.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 try:
-        from setuptools import setup
+    from setuptools import setup
 except ImportError:
-        from distutils.core import setup
+    from distutils.core import setup
 
 setup(name='pyThreadpool',
       version='0.1',
@@ -10,4 +10,16 @@ setup(name='pyThreadpool',
       author='Sriram Sundarraj',
       author_email='sriram.s.1994@gmail.com',
       license='MIT',
-      packages=['pyThreadpool'],)
+      packages=['pyThreadpool'],
+      classifiers=[
+                  'Operating System :: POSIX'
+                  'Topic :: Software Development :: Libraries :: Application Frameworks'
+                  'Intended Audience :: Developers'
+                  'Programming Language :: Python',
+                  'Programming Language :: Python :: 2',
+                  'Programming Language :: Python :: 2.7',
+                  'Programming Language :: Python :: 3',
+                  'Programming Language :: Python :: 3.2',
+                  'Programming Language :: Python :: 3.3',
+                  ],
+      )
